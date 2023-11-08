a99d675abca140d568848f4c8d87741479a6fce2
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-11-07 23:06:30 -0300

    corrindo erros de registro

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,16 +10,12 @@ const register = async (req, res) => {
     const dataIsvalid = validatorFieldFilled(data);
 
     if(dataIsvalid?.message){
-        return res.send(dataIsvalid)
-    }else{
-      return res.send(
-        `salvar dado no banco!`
-      )
+        return res.status(400).json(dataIsvalid)
     }
 
 
     try {
-        const encryptedPassword = await bcrypt.hash(password, 10);
+        const encryptedPassword = await bcrypt.hash(data.password, 10);
 
         const { rows } = await pool.query(
             `
@@ -29,12 +25,13 @@ const register = async (req, res) => {
             ($1, $2, $3, $4, $5)
             RETURNING *
             `,
-            [first_name, last_name, email, cpf, encryptedPassword]
+            [data.first_name, data.last_name,data.email,data.cpf, encryptedPassword]
         )
 
         return res.status(201).json(rows[0]);
 
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ message: 'Erro interno de servidor.' });
     }
 }
